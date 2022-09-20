9a3ac16a761ab0edf1abc5a646770d5ffef8e779
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-09-20 14:00:11 -0500

    [+] app.js: Seleccionar/Desseleccionar listado de skills formulario nueva vacante

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,32 @@
-console.log( 'Archivo precompilado con Webpack' );
+document.addEventListener( 'DOMContentLoaded', () => {
+    const skills = document.querySelector( '.lista-conocimientos' );
+
+    if( skills ) {
+        skills.addEventListener( 'click', addSkill );
+    }
+});
+
+
+const skills = new Set();
+
+const addSkill = event => {
+    // console.log( event.target );
+
+    /** Validamos el evento solo sobre los elementos LI */
+    if( event.target.tagName === 'LI' ) {
+
+        /** Elimina skill */
+        if( event.target.classList.contains( 'activo' ) ) {
+            event.target.classList.remove( 'activo' );
+            skills.delete( event.target.textContent );
+        }
+        else {  /** Si no agrega skill */
+            event.target.classList.add( 'activo' );
+            skills.add( event.target.textContent );
+        }
+
+    }
+
+    console.log( skills );
+    document.querySelector( '#skills' ).value = [ ...skills ];
+}

00611cd07ccabc65b94651bf51c8becdee772d22
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-09-21 14:22:31 -0500

    [+] View/Controller home: Desplegar listado de vacantes disponibles

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,10 +1,22 @@
-exports.showJobOffers = ( req, res ) => {
+const
+    mongoose = require( 'mongoose' ),
+    Vacant = mongoose.model( 'Vacancies' );
+
+exports.showJobOffers = async ( req, res, next ) => {
+    const vacancies = await Vacant.find().lean();   // ! lean: Omite la creación de instancias de un documento completo de Mongoose (Object Mongoose => Object POJO) 
+                                                    // !       https://mongoosejs.com/docs/tutorials/lean.html
+
+    if( ! vacancies ) return next();
+
+    // console.log( vacancies );
+
     res.render( 'home', {
         siteName: 'devJobs',
         namePage: 'Inicio',
         tagLine: 'Encuentra y Pública Trabajos para Desarrolladores Web',
         barra: true,
         boton: true,
-        session: JSON.stringify( req.session )
+        session: JSON.stringify( req.session ),
+        vacancies
     });
 }

ed9901dc0b7f41a44c845faf814489718dc047da
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-09-23 17:31:05 -0500

    [+] app.js: Eliminar las alertas 1 a 1 del DOM usando un intervalo de 2 segundos por elemento

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,10 @@
 document.addEventListener( 'DOMContentLoaded', () => {
-    const skills = document.querySelector( '.lista-conocimientos' );
+    const
+        skills = document.querySelector( '.lista-conocimientos' ),
+        alertsEl = document.querySelector( '.alertas' );
+
+    if( alertsEl )
+        clearAlerts( alertsEl );
 
     if( skills ) {
         skills.addEventListener( 'click', addSkill );
@@ -47,5 +52,25 @@ const selectedSkills = () => {
 
     console.log( skills );
     document.querySelector( '#skills' ).value = [ ...skills ];  // Inyecta los tados en el input type="hidden"
+
+}
+
+const clearAlerts = ( alerts ) => {
+
+    const intervalID = setInterval( () => {
+        /** Validamos si el elemento tiene elementos hijos */
+        if( alerts.children.length > 0 ) {
+            alerts.removeChild( alerts.children[ 0 ] );  // ! Elimina el primer hijo del elemento alerts
+            console.log( 'Elimina alerta' );
+        }
+        else {
+            clearInterval( intervalID );
+            intervalID = null;
+            console.log( 'Elimina setInterval & y elemento con clase alerta del DOM' );
+
+            alerts.parentElement.removeChild( alerts );
+        }
+
+    }, 2000 );  // ! Se ejecutara cada 2 segundos
 
 }

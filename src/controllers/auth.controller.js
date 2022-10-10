2c16edf5fd0a27ffaac688ce93bd752e663aa8fd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-10-10 17:58:48 -0500

    [$] Controller: Auth, mensaje error campos obligatorios

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,5 +4,6 @@ const passport = require( 'passport' );
 exports.authenticateUser = passport.authenticate( 'local', {
     successRedirect: '/ok',
     failureRedirect: '/iniciar-sesion',
-    failureFlash: true
+    failureFlash: true,
+    badRequestMessage: 'Ambos campos son obligatorios'
 });
